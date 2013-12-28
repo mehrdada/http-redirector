3c7ef573768d3548cb4245df83baf47f296c51d3
Mehrdad Afshari <mehrdada@users.noreply.github.com>
2013-12-28 03:25:20 -0500

    Bind to default port (80)

--- redirector.go
+++ redirector.go
@@ -10,5 +10,5 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, os.Args[1]+r.URL.RequestURI(), 302)
 	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe("", nil))
 }
